35a0de5b85c02043c4b6d1c1d47fe4461bcc0a8f
waristo <waristo.dev@gmail.com>
2021-07-19 18:14:37 +0900

    security fundamental : not yet jwt

--- src/main/java/warsito/musicweblibrary/entity/User.java
+++ src/main/java/warsito/musicweblibrary/entity/User.java
@@ -4,19 +4,27 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collection;
 
 @Data
 @Entity
-public class User {
+public class User implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
     @Id
-    private String id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @NotBlank
     @Size(min = 4, max = 10, message = "size of username : 4 to 10")
@@ -26,16 +34,39 @@ public class User {
     @NotBlank
     private String email;
 
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
+    private LocalDate createdAt;
+    private LocalDate modifiedAt;
 
     @Size(min = 8, max = 15, message = "size of password: 8 to 15")
     private String password;
 
-    @NotBlank
-    @Max(value = 10)
-    private int level;
-
     @PrePersist
-    void createdAt() { this.createdAt = LocalDateTime.now();}
+    void createdAt() {
+        this.createdAt = LocalDate.now();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
