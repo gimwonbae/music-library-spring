2af2f3141de964f5564e60528a86fbb3c1210879
waristo <waristo.dev@gmail.com>
2021-07-31 18:28:30 +0900

    change db setting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-insert into artist values (11, '2020-01-01', '2020-01-21', 'biggie1');
-insert into artist values (12, '2020-01-01', '2020-01-21', 'biggie2');
-insert into artist values (13, '2020-01-01', '2020-01-21', 'biggie3');
-insert into artist values (14, '2020-01-01', '2020-01-21', 'biggie4');
-insert into artist values (15, '2020-01-01', '2020-01-21', 'biggie5');
-insert into artist values (16, '2020-01-01', '2020-01-21', 'biggie6');
-insert into artist values (17, '2020-01-01', '2020-01-21', 'biggie7');
-insert into artist values (18, '2011-01-01', '2012-02-12', 'frank ocean');
-
-insert into album values(11, 'hiphop', 'ready to die1', 1, '2020-01-21', 11);
-insert into album values(12, 'hiphop', 'ready to die2', 2, '2021-01-21', 11);
-insert into album values(13, 'hiphop', 'ready to die3', 1, '2019-01-21', 13);
-insert into album values(14, 'pop', 'ready to die4', 0, '2018-01-21', 14);
-insert into album values(15, 'hiphop', 'ready to die5', 0, '1999-01-21',15);
+-- insert into artist values (11, '2020-01-01', '2020-01-21', 'biggie1');
+-- insert into artist values (12, '2020-01-01', '2020-01-21', 'biggie2');
+-- insert into artist values (13, '2020-01-01', '2020-01-21', 'biggie3');
+-- insert into artist values (14, '2020-01-01', '2020-01-21', 'biggie4');
+-- insert into artist values (15, '2020-01-01', '2020-01-21', 'biggie5');
+-- insert into artist values (16, '2020-01-01', '2020-01-21', 'biggie6');
+-- insert into artist values (17, '2020-01-01', '2020-01-21', 'biggie7');
+-- insert into artist values (18, '2011-01-01', '2012-02-12', 'frank ocean');
+--
+-- insert into album values(11, 'hiphop', 'ready to die1', 1, '2020-01-21', 11);
+-- insert into album values(12, 'hiphop', 'ready to die2', 2, '2021-01-21', 11);
+-- insert into album values(13, 'hiphop', 'ready to die3', 1, '2019-01-21', 13);
+-- insert into album values(14, 'pop', 'ready to die4', 0, '2018-01-21', 14);
+-- insert into album values(15, 'hiphop', 'ready to die5', 0, '1999-01-21',15);
