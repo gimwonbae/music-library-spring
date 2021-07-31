b12576c11793345afa5fa08a7cfd039492a3abce
waristo <waristo.dev@gmail.com>
2021-07-31 14:59:28 +0900

    dataset

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,8 +7,8 @@ insert into artist values (16, '2020-01-01', '2020-01-21', 'biggie6');
 insert into artist values (17, '2020-01-01', '2020-01-21', 'biggie7');
 insert into artist values (18, '2011-01-01', '2012-02-12', 'frank ocean');
 
-insert into album values(11, 'hiphop', 'ready to die1', 1, '2020-01-21', 1);
-insert into album values(12, 'hiphop', 'ready to die2', 2, '2021-01-21', 1);
-insert into album values(13, 'hiphop', 'ready to die3', 1, '2019-01-21', 3);
-insert into album values(14, 'pop', 'ready to die4', 0, '2018-01-21', 4);
-insert into album values(15, 'hiphop', 'ready to die5', 0, '1999-01-21',5);
+insert into album values(11, 'hiphop', 'ready to die1', 1, '2020-01-21', 11);
+insert into album values(12, 'hiphop', 'ready to die2', 2, '2021-01-21', 11);
+insert into album values(13, 'hiphop', 'ready to die3', 1, '2019-01-21', 13);
+insert into album values(14, 'pop', 'ready to die4', 0, '2018-01-21', 14);
+insert into album values(15, 'hiphop', 'ready to die5', 0, '1999-01-21',15);
